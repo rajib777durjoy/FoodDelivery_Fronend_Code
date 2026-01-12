abd0498fa37b82ca9c2e0ee724a02f50a82b4e1d
rajib777durjoy <rajib777chanda@gmail.com>
2026-01-12 23:04:50 +0600

    restaurant dashboard responsive done

--- src/Dashboard/RestaurantDashboard.jsx
+++ src/Dashboard/RestaurantDashboard.jsx
@@ -6,7 +6,7 @@ import { CiMenuFries } from "react-icons/ci";
 
 const RestaurantOwnerDashboard = () => {
     const { SignOutUser, user } = useContext(AuthContext);
-    const [menu, setMenu] = useState(true);
+    const [menu, setMenu] = useState(false);
     console.log('menu', menu)
     const linkClass = ({ isActive }) =>
         isActive
@@ -56,7 +56,7 @@ const RestaurantOwnerDashboard = () => {
                 </button>
             </div>
             {
-                menu ? <div className={`min-w-36 bg-gray-600 py-2 px-2 lg:hidden h-150 sticky top-0 rounded-br-md   ${menu && 'translate-x-0' || '-translate-x-48 '} `}>
+                menu ? <div className={`min-w-36 bg-gray-600 py-2 px-2 z-50 lg:hidden h-150 sticky top-0 rounded-br-md   ${menu && 'translate-x-0' || '-translate-x-48 '} `}>
                     <div className='px-4 py-4 flex items-center gap-2'>
                         <img src={user?.photoURL} className='w-6 h-6 rounded-full mt-1' alt="" />
                         <h1 className='text-xl'>Eatnow</h1>
@@ -100,8 +100,8 @@ const RestaurantOwnerDashboard = () => {
             {/* Main content */}
             <div className="flex-1 bg-white text-black">
                 {/* Top navbar (optional) */}
-                <div className="h-20 border-b flex items-center justify-between px-6">
-                    <h2 className="text-xl font-semibold">
+                <div className="h-20 border-b flex items-center justify-between px-2 lg:px-6">
+                    <h2 className="text-sm lg:text-xl font-semibold">
                         Restaurant Owner Panel
                     </h2>
                     <div className='lg:hidden'>
