2de338d578b55a73a4e279244d6a7135bf4a0e7a
rajib777durjoy <rajib777chanda@gmail.com>
2026-03-29 21:57:08 +0600

    uncomment axios api  hook

--- src/Public/Hook/useAxiosPublic.jsx
+++ src/Public/Hook/useAxiosPublic.jsx
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import React from 'react';
 
-// const instance = axios.create({
-//   baseURL:import.meta.env.VITE_HOSTING_URL,
-//   withCredentials:true
-// });
 const instance = axios.create({
-  baseURL:'http://localhost:5000',
+  baseURL:import.meta.env.VITE_HOSTING_URL,
   withCredentials:true
 });
+// const instance = axios.create({
+//   baseURL:'http://localhost:5000',
+//   withCredentials:true
+// });
 
 const useAxiosPublic = () => {
     return instance;
--- src/Public/Hook/useAxiosSecure.jsx
+++ src/Public/Hook/useAxiosSecure.jsx
@@ -3,14 +3,14 @@ import React, { useContext, useEffect } from 'react';
 import { AuthContext } from '../Provider/AuthProvider';
 import { useNavigate } from 'react-router';
 
-// const instance = axios.create({
-//     baseURL: import.meta.env.VITE_HOSTING_URL, withCredentials: true
-// });
-
 const instance = axios.create({
-  baseURL:'http://localhost:5000',
-  withCredentials:true
+    baseURL: import.meta.env.VITE_HOSTING_URL, withCredentials: true
 });
+
+// const instance = axios.create({
+//   baseURL:'http://localhost:5000',
+//   withCredentials:true
+// });
 
 const useAxiosSecure = () => {
     const {SignOutUser}=useContext(AuthContext)
